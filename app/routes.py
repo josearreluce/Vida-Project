38fcd7a109dd669f7656661d22f7868adfd9f826
Max Demers <mgdemers@uchicago.edu>
2018-11-10 15:35:44 -0600

    starting login form

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,16 @@
 from app import app
 from flask import render_template
+from app.forms import LoginForm
 
 @app.route("/assessment")
 def symptom_assessment():
     return render_template("assessment.html")
 
 @app.route("/")
-def main():
-    return render_template("login.html")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
+
 
 if __name__ == "__main__":
     app.run()
