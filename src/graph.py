f1d559afb77822c86162d93801f889bd6baa9368
Jose Arreluce <arreluce.jose@gmail.com>
2018-11-10 20:37:33 -0600

    Updated graph.py and graphTest.py to fix compilation errors and to match PEP8 style.

--- src/graph.py
+++ src/graph.py
@@ -6,11 +6,11 @@ import networkx as nx
 import symptom
 import condition
 
-#Note: hashability required for nodes - instances of user-defined classes are hashable, which we use.
-#Note: Can DEFINITELY be re-factored. Repetitive code is separate to emphasize different cases.
+# Note: hashability required for nodes - instances of user-defined classes are hashable, which we use.
+# Note: Can DEFINITELY be re-factored. Repetitive code is separate to emphasize different cases.
 
 
-### Error Functions: Optional for validity checking ###
+# Error Functions: Optional for validity checking ###
 
 def noSymptom(): #Symptoms doesn't exist
     print("Error. Nonexistent symptom found.")
@@ -19,94 +19,86 @@ def noSymptom(): #Symptoms doesn't exist
 def noCondition(): #Conditions doesn't exist
     print("Error. Nonexistent condition found.")
     return 0
-###                 ###
 
-#(1) Return an empty graph: For now, plain unweighted edges, with no clear distinction between symptom/condition nodes
+# (1) Return an empty graph: For now, plain unweighted edges, with no clear distinction between symptom/condition nodes
 def createGraph():
-  return nx.Graph()
+    return nx.Graph()
 
 
-#(2) Add a symptom to the graph, with related symptoms, conditions
-def addSymptom(G,s,symptoms,conditions):
-  G.add_node(s)
+# (2) Add a symptom to the graph, with related symptoms, conditions
+def addSymptom(G, s, symptoms, conditions):
+    G.add_node(s)
   
-  for i in symptoms:
-    G.add_edge(s,symptoms[i])
-  for i in conditions:
-    G.add_edge(s,conditions[i])
+    for i in range(len(symptoms)):
+        G.add_edge(s, symptoms[i])
+    for i in range(len(conditions)):
+        G.add_edge(s, conditions[i])
 
-  return G
-#
+    return G
 
 
-#(3) Add a condition to the graph, with related symptoms
-def addCondition(G,c,symptoms):
-  G.add_node(c)
+# (3) Add a condition to the graph, with related symptoms
+def addCondition(G, c, symptoms):
+    G.add_node(c)
   
-  for i in symptoms:
-    G.add_edge(c,symptoms[i])
+    for i in range(len(symptoms)):
+        G.add_edge(c,symptoms[i])
   
-  return G
-#
+    return G
 
 
-#(4) Get related conditions given a symptom
-def getRelatedConditions(G,symptom): #Get related conditions given a symptom
-  if ( (type(symptom) is not Symptom) ): #Make sure we're actually using a symptom
-    nosymptom()
+# (4) Get related conditions given a symptom
+def getRelatedConditions(G, symptom):  # Get related conditions given a symptom
+    if type(symptom) != 'Symptom':  # Make sure we're actually using a symptom
+        nosymptom()
     
-  conditions = []
-  neighbors = G.neighbors()
+    conditions = []
+    neighbors = G.neighbors()
   
-  for i in neighbors:
-    if (type(neighbors[i]) is Symptom): #Check the typing
-      conditions.append(neighbors[i])
+    for i in range(len(neighbors)):
+        if type(neighbors[i]) == 'Symptom':  # Check the typing
+            conditions.append(neighbors[i])
 
-  return conditions
-#
+    return conditions
 
 
-#(5) Get related symptom given a condition
-def getRelatedSymptomsC(G,condition):
-  if ( (type(condition) is not Condition) ): #Make sure we're actually given a condition
-    nocondition()
+# (5) Get related symptom given a condition
+def getRelatedSymptomsC(G, condition):
+    if type(condition) != 'Condition':  # Make sure we're actually given a condition
+        nocondition()
+
+    symptoms = []
+    neighbors = G.neighbors()
   
-  symptoms = []
-  neighbors = G.neighbors()
-  
-  for i in neighbors:
-    if (type(neighbors[i]) is Condition):
-      symptoms.append(neighbors[i])
+    for i in range(len(neighbors)):
+        if type(neighbors[i]) == 'Condition':
+            symptoms.append(neighbors[i])
 
-  return symptoms
-#
+    return symptoms
 
 
-#(6) Get related symptoms given a symptom
-def getRelatedSymptomsS(G,symptom): #Get related symptoms given a symptom
-  if ( (type(condition) is not Symptom) ): #Make sure we're actually given a condition
-    nocondition()
+# (6) Get related symptoms given a symptom
+def getRelatedSymptomsS(G, symptom):  # Get related symptoms given a symptom
+    if type(condition) != 'Symptom':  # Make sure we're actually given a condition
+        nocondition()
 
-  symptoms = []
-  neighbors = G.neighbors()
+    symptoms = []
+    neighbors = G.neighbors()
 
-  for i in neighbors:
-    if (type(neighbors[i]) is Symptom):
-      symptoms.append(neighbors[i])
+    for i in range(len(neighbors)):
+        if type(neighbors[i]) == 'Symptom':
+            symptoms.append(neighbors[i])
 
-  return symptoms
-#
-
-
-#(7) Remove a symptom, and all
-def removeSymptom(G,symptom): #Remove a symptom from the graph, deleting associated edges
-  G.remove_node(symptom)
-  return graph
-#
+    return symptoms
 
 
-#(8) Remove a condition from the graph, deleting associated edges
-def removeCondition(G,condition):
+# (7) Remove a symptom, and all
+def removeSymptom(G, symptom):  # Remove a symptom from the graph, deleting associated edges
+    G.remove_node(symptom)
+    return G
+
+
+# (8) Remove a condition from the graph, deleting associated edges
+def removeCondition(G, condition):
   G.remove_node(condition)
-  return graph
-#
+  return G
--- src/graphTest.py
+++ src/graphTest.py
@@ -7,141 +7,141 @@ import symptom as s
 
 import graph as g
 
-#Test Commposition : First tests for each function are very simple "sanity" tests: One involves a graph that will return
-#nothing when the function is called, one involves a simple case with one edge + two nodes, and the last test is a complex test using a
-#graph with multiple nodes and edges.
+# Test Commposition : First tests for each function are very simple "sanity" tests: One involves a graph that will return
+# nothing when the function is called, one involves a simple case with one edge + two nodes, and the last test is a complex test using a
+# graph with multiple nodes and edges.
 
-#NOTE(!): Ideally, we should check for equality between nodes instead of "number of neighbors" or "length of graph" to ensure correctness.
-#however adding Symptoms/Condition edges is very simple, and it is almost impossible to add wrong nodes/edges.
-#If there is a mistake, the number of neighbors will simply be off - otherwise, we have added exactly what we require.
+# NOTE(!): Ideally, we should check for equality between nodes instead of "number of neighbors" or "length of graph" to ensure correctness.
+# however adding Symptoms/Condition edges is very simple, and it is almost impossible to add wrong nodes/edges.
+# If there is a mistake, the number of neighbors will simply be off - otherwise, we have added exactly what we require.
 
-#Furthermore, we are *writing test cases* - in order to check for exact equality of nodes, we require a lot more involved implementation.
-#When we execute testing, we may test for exact nodes/edges, instead of number of neighbors, but we will use these test cases.
+# Furthermore, we are *writing test cases* - in order to check for exact equality of nodes, we require a lot more involved implementation.
+# When we execute testing, we may test for exact nodes/edges, instead of number of neighbors, but we will use these test cases.
 
-GG = g.createGraph() #Complex graph with 4 nodes - stronger test. #4 nodes, 4 edges (1 S-S edge, 3 S-C edges)
-#### Visual of the connections
-#C1-----S1
+GG = g.createGraph()  # Complex graph with 4 nodes - stronger test. #4 nodes, 4 edges (1 S-S edge, 3 S-C edges)
+# Visual of the connections
+# C1-----S1
 
-#C2-----(S1,S2)
-#S1-----S2
+# C2-----(S1,S2)
+# S1-----S2
 S1 = s.Symptom(1, 1, 1, 1, 1, 1)
 S2 = s.Symptom(2, 2, 2, 2, 2, 2)
 C1 = c.Condition(1, 1, 1, 1, 1)
 C2 = c.Condition(2, 2, 2, 2, 2)
 g.addSymptom(GG, S1, [], [])
 g.addSymptom(GG, S2, [S1], [])
-g.addCondition(GG, C1, [S1], [])
+g.addCondition(GG, C1, [S1])
 g.addCondition(GG, C2, [S1, S2])
 
 
-#createGraph test
+# createGraph test
 def createTest():
-  G = g.createGraph()
-  res = (len(G) == 0)
-  return res
+    G = g.createGraph()
+    res = len(G) == 0
+    return res
 
-#addSymptom test
+
+# addSymptom test
 def addSymptomSimple():
-  G = g.createGraph()
-  S1 = s.Symptom(1, 1, 1, 1, 1, 1)
-  S2 = s.Symptom(2, 2, 2, 2, 2, 2)
-  g.addSymptom(G, S1, [], [])
-  res1 = (len(G) == 1)
-  g.addSymptom(G, S2, [], [])
-  res2 = (len(G) == 2)
-  return res1 and res2
+    G = g.createGraph()
+    S1 = s.Symptom(1, 1, 1, 1, 1, 1)
+    S2 = s.Symptom(2, 2, 2, 2, 2, 2)
+    g.addSymptom(G, S1, [], [])
+    res1 = (len(G) == 1)
+    g.addSymptom(G, S2, [], [])
+    res2 = (len(G) == 2)
+    return res1 and res2
 
 def addWithEdge():
-  G = g.createGraph()
-  S1 = s.Symptom(1, 1, 1, 1, 1, 1)
-  S2 = s.Symptom(2, 2, 2, 2, 2, 2)
-  g.addSymptom(G, S1, [], [])
-  g.addSymptom(G, S2, [S1], [])
-  res1 = (len(G) == 2)
-  res2 = (len(G.edges()) == 1)
-  return res1 and res2
+    G = g.createGraph()
+    S1 = s.Symptom(1, 1, 1, 1, 1, 1)
+    S2 = s.Symptom(2, 2, 2, 2, 2, 2)
+    g.addSymptom(G, S1, [], [])
+    g.addSymptom(G, S2, [S1], [])
+    res1 = (len(G) == 2)
+    res2 = (len(G.edges()) == 1)
+    return res1 and res2
 
 def addComplex():
-  S3 = s.Symptom(1, 1, 1, 1, 1, 1)
-  g.addSymptom(GG, S3, [S1, S2], [C1, C2])
-  res1 = (len(GG) == 5)
-  res2 = (len(GG.edges()) == 7)
-  res3 = (len(GG[S3]) == 4)
-  res4 = (len(GG[S1]) == 4)
-  res5 = (len(GG[S2]) == 3)
-  res6 = (len(GG[C1]) == 2)
-  res7 = (len(GG[C2]) == 3)
-  return res1 and res2 and res3 and res4 and res5 and res6 and res7
+    S3 = s.Symptom(1, 1, 1, 1, 1, 1)
+    g.addSymptom(GG, S3, [S1, S2], [C1, C2])
+    res1 = (len(GG) == 5)
+    res2 = (len(GG.edges()) == 7)
+    res3 = (len(GG[S3]) == 4)
+    res4 = (len(GG[S1]) == 4)
+    res5 = (len(GG[S2]) == 3)
+    res6 = (len(GG[C1]) == 2)
+    res7 = (len(GG[C2]) == 3)
+    return res1 and res2 and res3 and res4 and res5 and res6 and res7
 
 #addCondition test
 def addConditionSimple():
-  G = g.createGraph()
-  C1 = c.Condition(1, 1, 1, 1, 1, 1)
-  C2 = c.Condition(2, 2, 2, 2, 2, 2)
-  g.addCondition(G, C1, [])
-  g.addCondition(G, C2, [])
-  res1 = (len(G) == 2)
-  return res1
+    G = g.createGraph()
+    C1 = c.Condition(1, 1, 1, 1, 1, 1)
+    C2 = c.Condition(2, 2, 2, 2, 2, 2)
+    g.addCondition(G, C1, [])
+    g.addCondition(G, C2, [])
+    res1 = (len(G) == 2)
+    return res1
 
 def addConditionEdge():
-  G = g.createGraph()
-  C1 = c.Condition(1, 1, 1, 1, 1, 1)
-  C2 = c.Condition(2, 2, 2, 2, 2, 2)
-  g.addCondition(G, C1, [])
-  g.addCondition(G, C2, [C1])
-  res1 = (len(G) == 2)
-  res2 = len(G.edges() == 1)
+    G = g.createGraph()
+    C1 = c.Condition(1, 1, 1, 1, 1, 1)
+    C2 = c.Condition(2, 2, 2, 2, 2, 2)
+    g.addCondition(G, C1, [])
+    g.addCondition(G, C2, [C1])
+    res1 = (len(G) == 2)
+    res2 = len(G.edges() == 1)
 
 #getRelatedConditions test
 def getNoRelatedSymptoms():
-  G = g.createGraph()
-  C1 = s.Symptom(1, 1, 1, 1, 1, 1)
-  S1 = s.Symptom(2, 2, 2, 2, 2, 2)
-  g.addCondition(G, C1, [])
-  g.addSymptom(G, S1, [], [])
-  related = g.getRelatedConditions(G, S1)
-  res1 = (len(related) == 0)
-  return res1
+    G = g.createGraph()
+    C1 = s.Symptom(1, 1, 1, 1, 1, 1)
+    S1 = s.Symptom(2, 2, 2, 2, 2, 2)
+    g.addCondition(G, C1, [])
+    g.addSymptom(G, S1, [], [])
+    related = g.getRelatedConditions(G, S1)
+    res1 = (len(related) == 0)
+    return res1
 
 def getSomeRelatedSymptoms():
-  G = g.createGraph()
-  C1 = s.Symptom(1, 1, 1, 1, 1, 1)
-  S1 = s.Symptom(2, 2, 2, 2, 2, 2)
-  g.addCondition(G, C1, [])
-  g.addSymptom(G, S1, [], [C1])
-  related = g.getRelatedConditions(G, S1)
-  res1 = (len(related) == 1)
-  return res1
+    G = g.createGraph()
+    C1 = s.Symptom(1, 1, 1, 1, 1, 1)
+    S1 = s.Symptom(2, 2, 2, 2, 2, 2)
+    g.addCondition(G, C1, [])
+    g.addSymptom(G, S1, [], [C1])
+    related = g.getRelatedConditions(G, S1)
+    res1 = (len(related) == 1)
+    return res1
 
 def getRelatedSymptomsComplex():
-
-  S3 = s.Symptom(1, 1, 1, 1, 1, 1)
-  C3 = c.Condition(1, 1, 1, 1, 1, 1)
-  g.addSymptom(GG, S3, [S1, S2], [C1, C2])
-  related = g.getRelatedConditions(GG, S3)
-  res1 = (len(related == 2))
-  related = g.getRelatedConditions(GG, S1)
-  res2 = (len(related == 2))
-  related = g.getRelatedConditions(GG, S2)
-  res3 = (len(related == 1))
+    S3 = s.Symptom(1, 1, 1, 1, 1, 1)
+    C3 = c.Condition(1, 1, 1, 1, 1, 1)
+    g.addSymptom(GG, S3, [S1, S2], [C1, C2])
+    related = g.getRelatedConditions(GG, S3)
+    res1 = (len(related == 2))
+    related = g.getRelatedConditions(GG, S1)
+    res2 = (len(related == 2))
+    related = g.getRelatedConditions(GG, S2)
+    res3 = (len(related == 1))
   
-  g.addCondition(GG, C3, [S1])
-  related = g.getRelatedConditions(GG, S1)
-  res4 = (len(related == 2))
+    g.addCondition(GG, C3, [S1])
+    related = g.getRelatedConditions(GG, S1)
+    res4 = (len(related == 2))
   
-  return res1 and res2 and res3 and res4
+    return res1 and res2 and res3 and res4
   
   
 #getRelatedSymptomsC test
 def getNoRelatedSymptomsC():
-  G = g.createGraph()
-  C1 = s.Symptom(1, 1, 1, 1, 1, 1)
-  S1 = s.Symptom(2, 2, 2, 2, 2, 2)
-  g.addCondition(G, C1, [])
-  g.addSymptom(G, S1, [], [])
-  related = g.getRelatedConditionsC(G, C1)
-  res1 = (len(related) == 0)
-  return res1
+    G = g.createGraph()
+    C1 = s.Symptom(1, 1, 1, 1, 1, 1)
+    S1 = s.Symptom(2, 2, 2, 2, 2, 2)
+    g.addCondition(G, C1, [])
+    g.addSymptom(G, S1, [], [])
+    related = g.getRelatedConditionsC(G, C1)
+    res1 = (len(related) == 0)
+    return res1
 
 def getSomeRelatedSymptomsC():
     G = g.createGraph()
