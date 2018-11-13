c42b8ae228c3cfd05ae4ff6995576968d1e4ed62
Jose Arreluce <arreluce.jose@gmail.com>
2018-11-12 22:14:15 -0600

    Fixed a minor bug.

--- app/forms.py
+++ app/forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 import sqlalchemy as sqlalchemy
 from sqlalchemy.orm import sessionmaker
-from src.db_user import User
+#from .src.db_user import User
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
@@ -16,10 +16,10 @@ class SignUpForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     sex = StringField('Sex', validators=[DataRequired()])
-    engine = sqlalchemy.create_engine("postgresql://pv_admin:CMSC22001@ec2-13-59-75-157.us-east-2.compute.amazonaws.com:5432/pv_db")
-    Session = sessionmaker()
-    Session.configure(bind=engine)
-    db = Session()
-    user_info = User(username, password)
-    db.add(user_info)
-    db.commit()
+    # engine = sqlalchemy.create_engine("postgresql://pv_admin:CMSC22001@ec2-13-59-75-157.us-east-2.compute.amazonaws.com:5432/pv_db")
+    # Session = sessionmaker()
+    # Session.configure(bind=engine)
+    # db = Session()
+    # user_info = User(username, password)
+    # db.add(user_info)
+    # db.commit()
