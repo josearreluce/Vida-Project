a03199e025216274555e377df7f556544cece309
Miles Grogger <mgrogger@pidgey.cs.uchicago.edu>
2018-11-13 16:36:02 -0600

    test_assessment.py updated

--- app/tests/test_assessment.py
+++ app/tests/test_assessment.py
@@ -2,6 +2,17 @@ import unittest
 import sys
 sys.path.append('../assessment') # gets into proper file path
 
-import assessment_test
+import assessment_simple_test as ast
 
 class TestAssessment(unittest.TestCase):
+
+    def setUp(self):
+        self.list_cond = ast.condition_list
+        self.graph = ast.state_network2
+        self.correct_symptom = "Symptom_1"
+        self.incorrect_symptom = "Symptom_3"
+        self.correct_condition = "Condition_1"
+        self.incorrect_conditon = "Condition_4"
+        self.correct_successors = ast.start_assessment(self.correct_symptom)
+
+
