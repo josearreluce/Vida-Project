7733d886b1565b008c5b6c92b9b879fbc41d9cfb
Miles Grogger <mgrogger@gmail.com>
2018-11-20 16:21:29 -0600

    updated test_db.py for iteration 2

--- app/tests/test_db.py
+++ app/tests/test_db.py
@@ -45,6 +45,17 @@ class TestQuery(unittest.TestCase):
 		self.no_value = "select * from conditions where sex=100"
 		self.right_value = "select name from conditions where cond_id='cond_1'"
 
+		# for test cases iteration 2
+		self.sub_right_val = "select sub_sympt_1 from related_symptoms where sympt_id='sympt_1'"
+		self.sub_wrong_val_no_probability = "select name from related_symptoms where sympt_id='sympt_1' and sub_sympt_1=0"
+		self.sub_wrong_val_certain_probability = "select name from related_symptoms where sympt_id='sympt_1' and (sub_sympt_5=1 or sub_sympt_1=1)"
+		self.sub_wrong_val_not_subsymptom = "select name from related_symptoms where sympt_id='sympt_2' and (sub_sympt_5!=0 or sub_sympt_1!=0)" 
+		self.sub_no_val = "select sub_sympt_120 from related symptoms where sympt_id='sympt_1'"
+		self.valid_age = "select name from conditions where cond_id = 'cond_1' and age='18-40'"
+		self.invalid_age = "select name from conditions where cond_id = 'cond_5' and age='18-40'"
+		self.valid_time = "select name from conditions where cond_id = 'cond_1' and time='3-5'"
+		self.invalid_time = "select name from conditions where cond_id = 'cond_5' and time='3-5'"
+
 	def test_right_query(self):
 		self.assertRaises(AssertionError, self.assertRaises, pg2.ProgrammingError, self.cur.execute, self.good_query)
 
@@ -64,4 +75,42 @@ class TestQuery(unittest.TestCase):
 		self.cur.execute(self.right_value)
 		value = self.cur.fetchall()[0][0]
 		self.assertEqual(value, 'apendicitis')
-		
+
+	#test cases iteration 2. This tests the general restructuring of the database that we imposed for this iteration
+
+	#there is no sub_sympt_120, therefore this should return None
+	def test_sub_no_val(self):
+		self.assertIsNone(self.cur.execute(self.sub_no_val))
+
+	#if sub_sympt_1 is a sub symptom of sympt_1, then ther is no condition where sympt_id='sympt_1' and sub_sympt_1=0
+	def test_sub_wrong_val_no_probability(self):
+		self.assertIsNone(self.cur.execute(self.sub_wrong_val_no_probability))
+
+	#if sub_sympt_5 and sub_symptom_1 are subsymptoms of sympt_1 then neither of them can have probability = 1
+	def test_sub_wrong_val_certain_probability(self):
+		self.assertIsNone(self.cur.execute(self.sub_wrong_val_certain_probability))
+
+	#if sub_sympt_5 and sub_symptom_1 are subsymptoms of sympt_1 then they cannot be subsymptoms of sympt_2, so they must = 0
+	def test_sub_wrong_val_not_subsymptom(self):
+		self.assertIsNone(self.cur.execute(self.sub_wrong_val_not_subsymptom))
+
+	def test_invalid_age(self):
+		self.assertIsNone(self.cur.execute(self.invalid_age))
+
+	def test_invalid_time(self):
+		self.assertIsNone(self.cur.execute(self.invalid_time))
+
+	def test_sub_right_val(self):
+		self.cur.execute(self.sub_right_val)
+		value = self.cur.fetchall()[0][0]
+		self.assertEqual(value, 0.5)
+
+	def test_valid_age(self):
+		self.cur.execute(self.valid_age)
+		value = self.cur.fetchall()[0][0]
+		self.assertEqual(value, "foo")
+
+	def test_valid_time(self):
+		self.cur.execute(self.valid_time)
+		value = self.cur.fetchall()[0][0]
+		self.assertEqual(value, "foo")
