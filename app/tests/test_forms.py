d9f543db65ac851c0842b581cd35455ff63802de
Max Demers <mgdemers@uchicago.edu>
2018-11-20 11:05:12 -0600

    reformatting TestWebForm into a superclass

--- app/tests/test_forms.py
+++ app/tests/test_forms.py
@@ -21,61 +21,67 @@ class TestWebForms(unittest.TestCase):
         self.login_page = '/'
         self.sign_up_page = '/sign_up'
 
-
-    def __delete_test_user(self):
+    def _delete_test_user(self):
             user_info = models.User(username=self.username, pswd=self.password)
             self.db.query(models.User).filter_by(username=self.username, pswd=self.password).delete()
             self.db.commit()
 
-    def __add_test_user(self):
+    def _add_test_user(self):
             u = models.User(username=self.username, pswd=self.password)
             self.db.add(u)
             self.db.commit()
 
-    def __make_post(self, page, username, password):
+    def _make_post(self, page, username, password):
         return self.app.post(page, data=dict(
             username=username,
             password=password
         ), follow_redirects=True)
 
-    def __test_user_in_db(self):
+    def _test_user_in_db(self):
         check_user = self.db.query(models.User).filter_by(username=self.username, pswd=self.password).count()
         return True if check_user >= 1 else False
 
+    def tearDown(self):
+        self.session.close()
+        return
+
+
+class TestSignUp(TestWebForms):
     def test_invalid_signup(self):
         # ensure the username will be invalid
-        if not self.__test_user_in_db():
-            self.__add_test_user()
+        if not self._test_user_in_db():
+            self._add_test_user()
 
         self.app.get(self.sign_up_page, follow_redirects=True)
-        response = self.__make_post(
+        response = self._make_post(
                 self.sign_up_page,
                 self.username,
                 self.password)
-
 
         self.assertIn('Username &#34;{}&#34; Already In Use!'.format(self.username), str(response.data))
 
     def test_valid_signup(self):
         # ensure username and pass are not already in the db
-        if self.__test_user_in_db():
-            self.__delete_test_user()
+        if self._test_user_in_db():
+            self._delete_test_user()
 
         self.app.get(self.sign_up_page, follow_redirects=True)
-        response = self.__make_post(
+        response = self._make_post(
                 self.sign_up_page,
                 self.username,
                 self.password)
 
         self.assertNotIn('Username &#34;{}&#34; Already In Use!'.format(self.username), str(response.data))
 
+
+class TestLogin(TestWebForms):
     def test_invalid_login(self):
         # ensure bad_username/pass are not saved already
-        if self.__test_user_in_db():
-            self.__delete_test_user()
+        if self._test_user_in_db():
+            self._delete_test_user()
 
         self.app.get(self.login_page, follow_redirects=True)
-        response = self.__make_post(
+        response = self._make_post(
                 self.login_page,
                 self.username,
                 self.password)
@@ -84,20 +90,17 @@ class TestWebForms(unittest.TestCase):
 
     def test_valid_login(self):
         # ensure username and pass are saved
-        if not self.__test_user_in_db():
-            self.__add_test_user()
+        if not self._test_user_in_db():
+            self._add_test_user()
 
         self.app.get(self.login_page,follow_redirects=True)
-        response = self.__make_post(
+        response = self._make_post(
                 self.login_page,
                 self.username,
                 self.password)
 
         self.assertNotIn("Invalid Username or Password", str(response.data))
 
-    def tearDown(self):
-        self.session.close()
-        return
 
 if __name__ == '__main__':
     unittest.main()
