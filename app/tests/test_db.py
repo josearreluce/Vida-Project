a732f839db6b0de6f6caf01b207eb0e8ca93836f
Miles Grogger <mgrogger@gmail.com>
2018-11-29 18:34:22 -0600

    updated test_db.py

--- app/tests/test_db.py
+++ app/tests/test_db.py
@@ -48,15 +48,16 @@ class TestQuery(unittest.TestCase):
 		self.right_value = "select name from conditions where cond_id='cond_1'"
 
 		# for test cases iteration 2
-		self.sub_right_val = "select sub_sympt_1 from related_symptoms where sympt_id='sympt_1'"
+		self.cond_right_val = "select sub_sympt_4 from conditions where cond_id='cond_4'"
+		self.sub_right_val = "select sub_sympt_21 from related_symptoms where sympt_id='sympt_1'"
 		self.sub_wrong_val_no_probability = "select name from related_symptoms where sympt_id='sympt_1' and sub_sympt_1=0"
 		self.sub_wrong_val_certain_probability = "select name from related_symptoms where sympt_id='sympt_1' and (sub_sympt_5=1 or sub_sympt_1=1)"
 		self.sub_wrong_val_not_subsymptom = "select name from related_symptoms where sympt_id='sympt_2' and (sub_sympt_5!=0 or sub_sympt_1!=0)" 
-		self.sub_no_val = "select sub_sympt_120 from related symptoms where sympt_id='sympt_1'"
-		self.valid_age = "select name from conditions where cond_id = 'cond_1' and age='18-40'"
-		self.invalid_age = "select name from conditions where cond_id = 'cond_5' and age='18-40'"
-		self.valid_time = "select name from conditions where cond_id = 'cond_1' and time='3-5'"
-		self.invalid_time = "select name from conditions where cond_id = 'cond_5' and time='3-5'"
+		self.sub_no_val = "select sub_sympt_120 from related_symptoms where sympt_id='sympt_1'"
+		self.valid_age = "select name from conditions where cond_id = 'cond_3' and age_min=0 and age_max=100"
+		self.invalid_age = "select name from conditions where cond_id = 'cond_5' and age_min=18 and age_max=40"
+		self.valid_time = "select name from conditions where cond_id = 'cond_3' and time_min=7 and time_max=10"
+		self.invalid_time = "select name from conditions where cond_id = 'cond_5' and time_min=3 and time_max=5"
 
 	def test_right_query(self):
 		self.assertRaises(AssertionError, self.assertRaises, pg2.ProgrammingError, self.cur.execute, self.good_query)
@@ -82,7 +83,7 @@ class TestQuery(unittest.TestCase):
 
 	#there is no sub_sympt_120, therefore this should return None
 	def test_sub_no_val(self):
-		self.assertIsNone(self.cur.execute(self.sub_no_val))
+		self.assertRaises(pg2.ProgrammingError, self.cur.execute, self.sub_no_val)
 
 	#if sub_sympt_1 is a sub symptom of sympt_1, then ther is no condition where sympt_id='sympt_1' and sub_sympt_1=0
 	def test_sub_wrong_val_no_probability(self):
@@ -102,17 +103,22 @@ class TestQuery(unittest.TestCase):
 	def test_invalid_time(self):
 		self.assertIsNone(self.cur.execute(self.invalid_time))
 
+	def test_cond_right_val(self):
+		self.cur.execute(self.cond_right_val)
+		value = self.cur.fetchall()[0][0]
+		self.assertEqual(value, 0.5)
+
 	def test_sub_right_val(self):
 		self.cur.execute(self.sub_right_val)
 		value = self.cur.fetchall()[0][0]
-		self.assertEqual(value, 0.5)
+		self.assertEqual(value, 0.125)
 
 	def test_valid_age(self):
 		self.cur.execute(self.valid_age)
 		value = self.cur.fetchall()[0][0]
-		self.assertEqual(value, "foo")
+		self.assertEqual(value, "common cold")
 
 	def test_valid_time(self):
 		self.cur.execute(self.valid_time)
 		value = self.cur.fetchall()[0][0]
-		self.assertEqual(value, "foo")
+		self.assertEqual(value, "common cold")
